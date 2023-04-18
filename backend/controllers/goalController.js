ef609bb1c92ea6314f830eefaa3cc0ca4b789403
Istvan Petrovan <petrovanistvan@gmail.com>
2023-04-18 13:53:21 +0200

    added middlewares

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,6 +9,8 @@ const getGoals = (req, res) => {
 // @route POST /api/goals
 // @access Private
 const setGoal = (req, res) => {
+  console.log(req.body);
+
   res.status(200).json({ message: "Set goal" });
 };
 
